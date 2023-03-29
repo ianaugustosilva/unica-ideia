191f37a3ab08b8eaac776a11e9dd536ed6d46ecf
Ian Augusto Silva <i.silva97augusto@gmail.com>
2023-03-29 13:35:29 -0300

    test getStaticProps

--- pages/tempo.js
+++ pages/tempo.js
@@ -1,12 +1,23 @@
-function Tempo() {
+function Tempo(props) {
 	const dynamicDate = new Date();
 	const dynamicDateString = dynamicDate.toGMTString();
 
 	return (
 		<div>
-			{dynamicDateString}
-			(dinâmico)
+			<div>{dynamicDateString}(dinâmico)</div>
+			<div>{props.staticDateString}(estático)</div>
 		</div>
 	);
 }
+export function getStaticProps() {
+	const staticDate = new Date();
+	const staticDateString = staticDate.toGMTString();
+
+	return {
+		props: {
+			staticDateString,
+		},
+	};
+}
+
 export default Tempo;
